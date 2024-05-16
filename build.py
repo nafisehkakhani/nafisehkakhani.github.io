6d73c679d5a8cf24e7fa91198e8dd28aa8594a49
NafeesK <nkakhani@gmail.com>
2024-05-16 11:49:32 +0200

    update

--- build.py
+++ build.py
@@ -33,7 +33,7 @@ def get_personal_data():
                 <p> Would like to connect? Feel free to contact me via mail!</p>
                 <p>
                     
-                    <a href="mailto:nafiseh-kakhani@uni-tuebingen.de" style="margin-right: 5px"><i class="far fa-envelope-open fa-lg"></i> Mail</a>
+                    <a href="mailto:nafiseh.kakhani@uni-tuebingen.de" style="margin-right: 5px"><i class="far fa-envelope-open fa-lg"></i> Mail</a>
                     <a href="https://twitter.com/NafisehKakhani" target="_blank" style="margin-right: 5px"><i class="fab fa-twitter fa-lg"></i> Twitter</a>
                     <a href="https://scholar.google.com/citations?user=b7EX1rUAAAAJ&hl=en" target="_blank" style="margin-right: 5px"><i class="fa-solid fa-book"></i> Scholar</a>
                     <a href="https://github.com/nafisehkakhani" target="_blank" style="margin-right: 5px"><i class="fab fa-github fa-lg"></i> Github</a>
